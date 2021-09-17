c9612a39ccea76b59cba9c759e1502db3da021eb
Codefident <piotr.klep01@wp.pl>
2021-09-17 23:06:32 +0200

    basic socket

--- jupipoly-client-app/src/index.tsx
+++ jupipoly-client-app/src/index.tsx
@@ -1,14 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { io } from "socket.io-client";
+
+const socket = io("http://127.0.0.1:3300");
+socket.on("connection", () => {});
+socket.emit("test-emit", "hello");
+socket.on("test-res", (res) => {});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
