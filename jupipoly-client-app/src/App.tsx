c9c26174282a86783a5c7ad2966c06d37665167c
Codefident <piotr.klep01@wp.pl>
2021-09-17 23:07:08 +0200

    added mui

--- jupipoly-client-app/src/App.tsx
+++ jupipoly-client-app/src/App.tsx
@@ -2,24 +2,21 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { createTheme } from '@mui/material';
+import { purple, blue } from '@mui/material/colors';
+
+import LoginForm from './components/LoginForm';
+
+const theme = createTheme({
+  palette: {
+    primary: purple,
+    secondary: blue
+  }
+})
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoginForm theme={theme}/>
   );
 }
 
---
+++ jupipoly-client-app/src/components/LoginForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { TextField, Button } from "@mui/material";
+
+export default function LoginForm(props: any) {
+    //const []
+  return (
+    <>
+      <TextField label="Nickname" variant="filled" />
+    </>
+  );
+}
